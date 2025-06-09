bc721c53604a2933c1ba8b474f781dff4ea5c8cf
Malik Mahmud <almalikmahmud@gmail.com>
2025-06-09 11:41:36 +0100

    feat: handle concurrent clients

--- app/main.go
+++ app/main.go
@@ -19,23 +19,8 @@ const (
 	PING = "PING"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	reader := bufio.NewReader(conn)
+func handleConnection(c net.Conn) {
+	reader := bufio.NewReader(c)
 
 	for {
 		line, err := reader.ReadString('\n')
@@ -50,7 +35,28 @@ func main() {
 		}
 
 		if strings.TrimSpace(line) == PING {
-			conn.Write([]byte("+PONG\r\n"))
+			c.Write([]byte("+PONG\r\n"))
 		}
+	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
